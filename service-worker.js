1d5cc8a16ab4365b909f18004627698c1e2b7aab
saygilifatih <saygilifatih93@gmail.com>
2026-09-22 13:37:18 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const C='bol-winst-v2',F=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
